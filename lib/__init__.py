977313e5e52ea2ced6e0fe6c63ad3ab06a1487d0
Scott Powers <scott.powers@untsystem.edu>
2015-07-10 12:40:54 -0500

    worker now references shared queue and has some structure to process jobs and report results

---
+++ lib/__init__.py
@@ -0,0 +1,7 @@
+try:
+  import Queue as queue
+except:
+  import queue
+
+
+work_q = queue.Queue()
--- lib/worker.py
+++ lib/worker.py
@@ -10,17 +10,28 @@ class Fetcher(threading.Thread):
   def __init__(self):
     threading.Thread.__init__(self)
     self.stop = threading.Event()
-    self.sleep = threading.Event()
     self.status_lock = threading.Lock()
-    self.work_q = queue.Queue()
     self.status = ""
     self.status_time = -1
-  def job_add(self,url):
-    pass
+    
+    self.results = {}
+    self.results_lock = threading.Lock()
+
+  def set_next_purge(self):
+    self.next_purge = time.time() + 120
+
+  def job_update(self,url,data):
+    with self.results_lock:
+      pass
+
   def job_status(self,url):
-    pass
+    with self.results_lock:
+      pass
+
   def job_results(self,url):
-    pass
+    with self.results_lock:
+      pass
+
   def set_status(self,text):
     with self.status_lock:
       self.status = text
@@ -31,11 +42,26 @@ class Fetcher(threading.Thread):
       return (self.status,self.status_time)
 
   def run(self):
+    self.set_next_purge()
     self.set_status("Worker Startup")
     while not self.stop.is_set():
-      self.sleep.wait(0.1)
-      self.sleep.clear()
-      #self.sleep.set()
+
+      #purged old results
+      if time.time() > self.next_purge:
+        for key in self.results:
+          pass
+        self.set_next_purge()
+
+      #get a url from the queue to work on 
+      try:
+        self.url_to_process = lib.work_q.get(block=True,timeout=0.25)
+      except queue.Empty:
+        continue
+
+      #process job here
+      #add results to job dict
+
+      self.set_status("Fetching:" + self.url_to_process)
       if self.stop.is_set(): break
     self.set_status("Worker Shutdown")
     
